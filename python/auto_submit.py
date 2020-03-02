281429db4dd65250f770962f2a2f4a4bd28e69b8
Anton Eriksson <anton.eriksson@reaktor.com>
2020-03-02 18:48:38 +0200

    Add python folder with requests setup

---
+++ python/auto_submit.py
@@ -0,0 +1,7 @@
+import requests
+
+response = requests.get("http://fuffens.herokuapp.com")
+
+print(response.text)
+
+# Add the code for filling in the form, https://requests.readthedocs.io/ might be useful.  
